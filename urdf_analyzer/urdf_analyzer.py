7d58e8e44e8d9116323ada1d7d856d390ad1309c
Daniella Tola <dani-ella95@live.com>
2022-10-21 16:42:25 +1000

    initial commit

---
+++ urdf_analyzer/urdf_analyzer.py
@@ -0,0 +1,10 @@
+# Script related packages
+import os
+from pathlib import Path
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+# URDF related packages
+import urdfpy
+import yourdfpy
+import roboticstoolbox as rtb
